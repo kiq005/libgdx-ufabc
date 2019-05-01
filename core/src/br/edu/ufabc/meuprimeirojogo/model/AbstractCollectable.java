f5520f788a96f96df11b2da64646bb926e0f9728
Lucas Zanferrari <lucas.zanferrari@gmail.com>
2019-05-01 17:09:46 -0300

    adding collectables (only potion for now)

---
+++ core/src/br/edu/ufabc/meuprimeirojogo/model/AbstractCollectable.java
@@ -0,0 +1,40 @@
+package br.edu.ufabc.meuprimeirojogo.model;
+
+import com.badlogic.gdx.math.Vector3;
+
+import br.edu.ufabc.meuprimeirojogo.core.GameObject;
+
+public abstract class AbstractCollectable extends AbstractModel {
+	
+	public int state;
+
+	public GameObject collectables[];
+	
+	public static final int UNCOLLECTED = 0;
+	public static final int COLLECTED   = 1;
+	
+	public AbstractCollectable(boolean collidable, boolean moveable) {
+		super(collidable, moveable);
+	}
+	
+	@Override
+	public GameObject getGameObject() {
+		return collectables[state];
+	}
+	
+ 	public void setPosition(float x, float y, float z){
+		for (int i = 0; i < collectables.length; i++) {
+			collectables[i].transform.translate(x, y, z);
+		}
+	}
+ 	
+ 	public Vector3 getPosition() {
+ 		Vector3 collectablePosition = new Vector3();
+ 		this.getGameObject().transform.getTranslation(collectablePosition);
+ 		return collectablePosition;
+ 	}
+ 	
+	public abstract void applyCollectableEffect();
+	public abstract void update(float delta);
+	
+}
